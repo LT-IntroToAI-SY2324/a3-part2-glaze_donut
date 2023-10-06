125079e7f9f97b129bde149dc57b956a4db0de38
Miles Lambert <mlambert2@cps.edu>
2023-10-06 11:58:06 -0500

    intial

---
+++ games.py
@@ -0,0 +1,15 @@
+games_db: List[Tuple[str, List[str], int, str]] = [
+    (
+        "137-126",  
+        [
+        "federico fellini",
+          # director
+        ]
+        2000,  # year
+        [
+            "magali noel",
+            "bruno zanin",
+            "pupella maggio",
+            "armando drancia",
+        ],  # actors/actresses
+    ),
