e88f9f61fb08415a45fb7a4b7abe93f3da62b6ea
Bruno Pena-Lepe <bpena-lepe@cps.edu>
2023-10-11 11:28:12 -0500

    gets

--- Code.py
+++ Code.py
@@ -3,20 +3,21 @@ from match import match
 from typing import List, Tuple, Callable, Any
 
 # The projection functions, that give us access to certain parts of a "movie" (a tuple)
-def get_title(movie: Tuple[str, str, int, List[str]]) -> str:
-    return movie[0]
+def get_score(game: Tuple[str, str, str, int, List[str]]) -> str:
+    return game[0]
 
-
-
-def get_director(movie: Tuple[str, str, int, List[str]]) -> str:
-    return movie[1]
+def get_winner(game: Tuple[str, str, str, int, List[str]]) -> str:
+    return game[1]
 
 # for movie in movie_db:
 #     print(get_director(movie))
 
-def get_year(movie: Tuple[str, str, int, List[str]]) -> int:
-    return movie[2]
+def get_loser(game: Tuple[str, str, str, int, List[str]]) -> str:
+    return game[2]
+
+def get_year(game: Tuple[str, str, str, int, List[str]]) -> int:
+    return game[3]
 
 
-def get_actors(movie: Tuple[str, str, int, List[str]]) -> List[str]:
-    return movie[3]
+def get_captains(game: Tuple[str, str, str, int, List[str]]) -> List[str]:
+    return game[4]
