8f9fd069ab6fe7b69d9d836b3f97767bc17f38b8
Bruno Pena-Lepe <bpena-lepe@cps.edu>
2023-10-19 11:09:59 -0500

    palist

--- Code.py
+++ Code.py
@@ -109,17 +109,6 @@ pa_list: List[Tuple[List[str], Callable[[List[str]], List[Any]]]] = [
 
 
 def search_pa_list(src: List[str]) -> List[str]:
-    """Takes source, finds matching pattern and calls corresponding action. If it finds
-    a match but has no answers it returns ["No answers"]. If it finds no match it
-    returns ["I don't understand"].
-
-    Args:
-        source - a phrase represented as a list of words (strings)
-
-    Returns:
-        a list of answers. Will be ["I don't understand"] if it finds no matches and
-        ["No answers"] if it finds a match but no answers
-    """
     for pat, act in pa_list:
         mat = match(pat, src)
         if mat is not None:
